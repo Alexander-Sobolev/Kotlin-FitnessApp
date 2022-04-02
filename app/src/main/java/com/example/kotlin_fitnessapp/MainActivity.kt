e2ed398ea58f59a9736149efc020dd014339f6a3
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-04-02 21:38:24 +0300

    Added RecyclerView and progressBar

--- app/src/main/java/com/example/kotlin_fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_fitnessapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kotlin_fitnessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_fitnessapp.fragments.DaysFragment
+import com.example.kotlin_fitnessapp.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FragmentManager.setFragment(DaysFragment.newInstance(), this)
     }
 }
